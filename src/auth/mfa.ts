04ce81b2479297494845b39f0c46829714bbd31e
ayazismailhakki <ayazismailhakki@gmail.com>
2020-05-09 09:33:26 +0300

    feat: mfa code validation method created

--- src/auth/mfa.ts
+++ src/auth/mfa.ts
@@ -5,5 +5,10 @@ export class MFAService extends HttpService {
         super(baseURL);
     }
 
+    public async validateCode(code: string, method: string) {
+        return this.http.post('/signin/challenge/' + method, {
+            code
+        });
+    }
 
 }

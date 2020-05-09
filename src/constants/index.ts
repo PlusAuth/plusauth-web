8284c2ba7228eab6ebfc85f835589e8733f965d8
ayazismailhakki <ayazismailhakki@gmail.com>
2020-05-09 12:47:20 +0300

    feat: verification token type constant created

---
+++ src/constants/index.ts
@@ -0,0 +1,1 @@
+export * from './verification_token_type'
---
+++ src/constants/verification_token_type.ts
@@ -0,0 +1,7 @@
+export enum VerificationTokenType {
+  EMAIL = 'email',
+  FORGOT_PASSWORD = 'forgot_password',
+  FORGOT_PASSWORD_CODE = 'forgot_password_code',
+  GA_SECRET = 'ga_secret',
+  INVITE_ADMIN = 'invite_admin'
+}

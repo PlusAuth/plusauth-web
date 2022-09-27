9fadd5e4a9c2abf0846e8b274254cd9bf6fbe86d
Ismail H. Ayaz <ayazismailhakki@gmail.com>
2022-09-27 16:25:27 +0300

    chore: migrate to vite

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import {resolve} from 'path'
+import {defineConfig} from 'vite'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  build: {
+    emptyOutDir: true,
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'plusauthweb',
+      // the proper extensions will be added
+      fileName: 'plusauth-web'
+    },
+    minify: 'esbuild',
+    sourcemap: true,
+    rollupOptions: {
+      output: [
+        {
+          format: 'cjs',
+          entryFileNames: "plusauth-web.js",
+        },
+        {
+          format: 'es',
+          entryFileNames: "plusauth-web.[format].js",
+        },
+        {
+          format: 'umd',
+          entryFileNames: "plusauth-web.[format].js",
+        },
+      ]
+    }
+  },
+  plugins: [
+    dts({
+      outputDir: 'types'
+    })
+  ]
+})
